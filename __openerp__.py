a21fb346d620802a1a7de020581050d7f0fb22b4
Sylvain GARANCHER <sylvain.garancher@syleam.fr>
2016-04-07 12:23:05 +0200

    [INIT] Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    stock_scanner_test_barcode module for OpenERP, A simple barcode scanning test scenario for the stock_scanner module
+#    Copyright (C) 2016 SYLEAM Info Services (<http://www.Syleam.fr/>)
+#              Sylvain Garancher <sylvain.garancher@syleam.fr>
+#
+#    This file is a part of stock_scanner_test_barcode
+#
+#    stock_scanner_test_barcode is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    stock_scanner_test_barcode is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Stock Scanner Test Barcode',
+    'version': '1.0',
+    'category': 'Custom',
+    'description': """A simple barcode scanning test scenario for the stock_scanner module""",
+    'author': 'SYLEAM',
+    'website': 'http://www.syleam.fr/',
+    'depends': [
+        'stock_scanner',
+    ],
+    'data': [
+        'data/Tests.scenario',
+        'data/Barcode/Barcode.scenario',
+    ],
+    'installable': True,
+    'active': False,
+    'license': 'AGPL-3',
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
